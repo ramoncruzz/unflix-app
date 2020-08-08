bb75113ea66ccdf6a7a6e003f9599f650c1ff3aa
C1296849 Ramon Cruz da Silva <c1296849@interno.bb.com.br>
2020-08-08 12:45:48 -0300

    instalacao react-native-vector-icons

--- App.js
+++ App.js
@@ -8,14 +8,27 @@
 
 import React from 'react';
 import { SafeAreaView, Text, StatusBar } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const App = () => (
   <SafeAreaView
     style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
   >
     <StatusBar backgroundColor="orange" />
-    <Text> UNFLIX </Text>
-    <Text> Aplicativo de filmes </Text>
+    <Text
+      style={{
+        fontFamily: 'BebasNeue-Regular',
+        fontSize: 60,
+        fontWeight: 'bold',
+      }}
+    >
+      {' '}
+      UNFLIX{' '}
+    </Text>
+    <Text style={{ fontFamily: 'SourceSansPro-ExtraLightItalic' }}>
+      Aplicativo de filmes
+    </Text>
+    <Icon name="rocket" size={30} color="#900" />
   </SafeAreaView>
 );
 export default App;
