cf6dfecbf42d186fea4621ead2fbf5a7884b7e8a
C1296849 Ramon Cruz da Silva <c1296849@interno.bb.com.br>
2020-08-08 13:54:44 -0300

    Configuracao animacoes lottie

--- App.js
+++ App.js
@@ -1,14 +1,9 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import { SafeAreaView, Text, StatusBar } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import LottieView from 'lottie-react-native';
+
+const loadding = require('./lottiefiles/world.json');
 
 const App = () => (
   <SafeAreaView
@@ -29,6 +24,7 @@ const App = () => (
       Aplicativo de filmes
     </Text>
     <Icon name="rocket" size={30} color="#900" />
+    <LottieView source={loadding} autoPlay loop={false} />
   </SafeAreaView>
 );
 export default App;
