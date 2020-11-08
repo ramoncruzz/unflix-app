09896df3a281fb8fe2152775469f119ce875dc2f
Ramon Cruz <ramon.silva@aisdigital.com.br>
2020-11-08 00:24:02 -0300

    refactor: 💡 Configuracoes
    configurando o Typscript

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,14 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
-    'module-resolver',{
+    'module-resolver',
+    {
       root: ['./src'],
       extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
       alias: {
-        "tests": ["./tests/"],
-        "@components": "./src/components",
-      }
-    }
-  ]
+        tests: ['./tests/'],
+        '@components': './src/components',
+      },
+    },
+  ],
 };
